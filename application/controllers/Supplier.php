1e77e7e679030a1746f8fa9f77fcc44c7ab58e20
Eko Hidayat <nc.ekohidayat@gmail.com>
2024-07-08 19:24:42 +0800

    [+] Supplier alkon

--- application/controllers/Supplier.php
+++ application/controllers/Supplier.php
@@ -14,10 +14,45 @@ class Supplier extends CI_Controller {
 	{
 		$data['title'] = $this->title;
 		$data['session'] = (object)$this->session;
+		$data['supplier'] = $this->M_supplier->get_all();
 		$this->load->view('template/header',$data);
 		$this->load->view('template/navbar',$data);
 		$this->load->view('template/topbar',$data);
 		$this->load->view('main/supplier',$data);
 		$this->load->view('template/footer',$data);
+	}
+
+	public function proses()
+	{
+		$data = array(
+			'nama_supplier' => $this->input->post('nama'),
+			'alamat_supplier' => $this->input->post('alamat')
+		);
+
+		$proses = $this->M_supplier->insert($data);
+
+		// if ($proses == true) {
+		// 	$this->session->set_flashdata('success', 'User Baru berhasil dibuat');
+		// } else {
+		// 	$this->session->set_flashdata('error', 'User Baru gagal dibuat');
+		// }
+
+		redirect('supplier');
+	}
+
+	public function delete($id)
+	{
+
+		$getid = $this->encryption->decrypt(urldecode($id));
+
+		$delete = $this->M_supplier->delete($getid);
+
+		// if ($delete == true) {
+		// 	$this->session->set_flashdata('success', 'Password berhasil direset');
+		// } else {
+		// 	$this->session->set_flashdata('error', 'Password gagal direset');
+		// }
+
+		redirect('supplier');
 	}
 }
--- application/models/SupplierModel.php
+++ application/models/SupplierModel.php
@@ -2,5 +2,33 @@
 
 class SupplierModel extends CI_Model
 {
+	public function get_all()
+	{
+		$query = $this->db->get('tbl_mst_supplier');
+		return $query->result();
+	}	
+
+	public function insert($data)
+	{
+		$this->db->insert('tbl_mst_supplier', $data);
+
+		if ($this->db->affected_rows() > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public function delete($id)
+	{
+		$this->db->where('id_supplier', $id);
+		$this->db->delete('tbl_mst_supplier');
+
+		if ($this->db->affected_rows() > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 	
 }
